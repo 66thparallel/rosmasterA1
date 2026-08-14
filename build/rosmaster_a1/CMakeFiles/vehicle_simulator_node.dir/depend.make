e2917cd186ab90c82e28b2b8b5460117701b023c
Jane L. <38669412+66thparallel@users.noreply.github.com>
2026-08-13 20:43:34 -0700

    Delete build directory

--- build/rosmaster_a1/CMakeFiles/vehicle_simulator_node.dir/depend.make
+++
@@ -1,2 +0,0 @@
-# Empty dependencies file for vehicle_simulator_node.
-# This may be replaced when dependencies are built.
--- build/rosmaster_a1/colcon_command_prefix_test.sh
+++
@@ -1,2 +0,0 @@
-# generated from colcon_core/shell/template/command_prefix.sh.em
-. "/home/janeikeliu/projects/rosmasterA1/install/rosmaster_a1/share/rosmaster_a1/package.sh"
